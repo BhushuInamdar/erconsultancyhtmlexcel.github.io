41e24e54fb5b54261599cd62bc35182819a42c69
BhushuInamdar <128927352+BhushuInamdar@users.noreply.github.com>
2023-04-14 17:41:55 +0530

    Add files via upload

---
+++ imp.js
@@ -0,0 +1,106 @@
+const excel_file = document.getElementById('excel_file');
+var flag = 0;
+var j = 0;
+excel_file.addEventListener('change', (event) => {
+    var reader = new FileReader();
+    reader.readAsArrayBuffer(event.target.files[0]);
+    reader.onload = function (event) {
+        var data = new Uint8Array(reader.result);
+        var work_book = XLSX.read(data, { type: 'array' });
+        var sheet_name = work_book.SheetNames;
+        var sheet_data = XLSX.utils.sheet_to_json(work_book.Sheets[sheet_name[0]], { header: 1 });
+        if (sheet_data.length > 0) {
+            var table_output = '<table class = "table table-striped table-bordered">';
+            for (var row = 0; row < sheet_data.length; row++) {
+                table_output += '<tr>';
+                for (var cell = 0; cell < sheet_data[row].length; cell++) {
+                    table_output += '<td>' + sheet_data[row][cell] + '</td>';
+                }
+
+                table_output += '</tr>';
+            }
+            table_output += '</table>';
+
+            document.getElementById('excel_data').innerHTML = table_output;
+        }
+    }
+});
+console.log(flag);
+const searchFunimpmore = () => {
+    let filter = document.getElementById('myInputimpmore').value.toUpperCase();
+    let myTable = document.getElementById('excel_data');
+    let tr = myTable.getElementsByTagName('tr');
+    for (var i = 0; i < tr.length; i++) {
+        let td4 = tr[i].getElementsByTagName('td')[8];
+        // let td5 = tr[i].getElementsByTagName('td')[8];
+        if (td4) {
+            let textvalue3 = td4.textContent || td4.innerHTML;
+            if (textvalue3.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "none";
+                flag = 1;
+                j = i;
+                console.log(j);
+            }
+            else{
+                tr[i].style.display = "";
+            }
+            console.log(j);   
+        }
+    }
+}
+
+const searchFun = () => {
+    let filter = document.getElementById('myInput').value.toUpperCase();
+    let myTable = document.getElementById('excel_data');
+    let tr = myTable.getElementsByTagName('tr');
+    for (var i = 0; i < tr.length; i++) {
+        let td1 = tr[i].getElementsByTagName('td')[2];
+        let td2 = tr[i].getElementsByTagName('td')[3];
+        if (td1) {
+            let textvalue1 = td1.textContent || td1.innerHTML;
+            console.log(j);
+            if(i != j){
+                if (textvalue1.toUpperCase().indexOf(filter) > -1) {
+                    tr[i].style.display = "";
+                }
+                else if (td2) {
+                    let textvalue2 = td2.textContent || td2.innerHTML;
+                    if (textvalue2.toUpperCase().indexOf(filter) > -1) {
+                        tr[i].style.display = "";
+                    }
+                    else {
+                        tr[i].style.display = "none";
+                    }
+                //    git add . then git commit -m "message" then git push origin master
+            }   
+            }
+            else{
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
+
+const searchFunimp = () => {
+    let filter = document.getElementById('myInputimp').value.toUpperCase();
+    let myTable = document.getElementById('excel_data');
+    let tr = myTable.getElementsByTagName('tr');
+    for (var i = 0; i < tr.length; i++) {
+        let td3 = tr[i].getElementsByTagName('td')[7];
+        if (td3) {
+            let textvalue1 = td3.textContent || td3.innerHTML;
+            if(i!=j){
+                if (textvalue1.toUpperCase().indexOf(filter) > -1) {
+                    tr[i].style.display = "";
+                }
+                else {
+                     tr[i].style.display = "none";
+                }
+                //    git add . then git commit -m "message" then git push origin master
+            } 
+        }
+        else{
+            tr[i].style.display = "none";
+        }
+    }
+}
